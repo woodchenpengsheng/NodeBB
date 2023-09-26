5fa2d1841f9f26a97d3ca7bf4530121fe2053a2a
wood <woodchenchen@gmail.com>
2023-09-27 01:13:51 +0800

    feat:优化充值跳转提示

--- public/src/client/topic/unlockContact.js
+++ public/src/client/topic/unlockContact.js
@@ -10,22 +10,41 @@ define('forum/topic/unlockContact', [
 			return;
 		}
 		topicContainer.on('click', '[component="topic/unlock-contact"]', function () {
-			const execute = function (ok) {
-				if (!ok) {
-					return;
-				}
-				api.put(`/topics/${tid}/unlockContact`, {})
-					.then((currentReputation) => {
-						alerts.success(`解锁成功，您现在的声望值为：${currentReputation}，2秒后自动刷新当前页面`);
-						setTimeout(() => {
-							window.location.reload();
-						}, 4000);
-					})
-					.catch(alerts.error);
-			};
+			if (!ajaxify.data.loggedIn) {
+				alerts.error('您尚未登陆，6秒后自动跳转到登陆界面，登陆成功后进行解锁。如果尚未注册，需要先点下右下角的小人注册按钮，完成注册哦。', 6000);
+				setTimeout(() => {
+					window.location.href = config.relative_path + '/login';
+				}, 6000);
+			} else {
+				const execute = function (ok) {
+					if (!ok) {
+						return;
+					}
+					api.put(`/topics/${tid}/unlockContact`, {})
+						.then((currentReputation) => {
+							alerts.success(`解锁成功，您现在的声望值为：${currentReputation}，3秒后自动刷新当前页面`);
+							setTimeout(() => {
+								window.location.reload();
+							}, 3000);
+						})
+						.catch(alerts.error);
+				};
 
-			const message = '[[topic:topic_unlock_contact,' + $('[component="topic/consume-reputation"]').text() + ']]';
-			bootbox.confirm(message, execute);
+				const message = '[[topic:topic_unlock_contact,' + $('[component="topic/consume-reputation"]').text() + ']]';
+				bootbox.confirm(message, execute);
+			}
+			return false;
+		});
+
+		topicContainer.on('click', '[component="recharge/reputation"]', function () {
+			if (!ajaxify.data.loggedIn) {
+				alerts.error('您尚未登陆，6秒后自动跳转到登陆界面，登陆后才能充声望。如果尚未注册，需要先点下右下角的小人注册按钮，完成注册哦。', 6000);
+				setTimeout(() => {
+					window.location.href = config.relative_path + '/login';
+				}, 6000);
+			} else {
+				window.location.href = config.relative_path + '/recharge';
+			}
 			return false;
 		});
 	};
